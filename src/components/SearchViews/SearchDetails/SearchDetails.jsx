e2a0e2dd9c131edf4d47b2f1a33e2fbb47495ab4
Heidi Ganac Nicholson <heidiganac@gmail.com>
2023-05-21 23:16:28 -0500

    mapped over sunlight array to display all options on search list

--- src/components/SearchViews/SearchDetails/SearchDetails.jsx
+++ src/components/SearchViews/SearchDetails/SearchDetails.jsx
@@ -8,7 +8,6 @@ function SearchDetails({ item }) {
 
   const history = useHistory();
   const dispatch = useDispatch();
-  const searchDetails = useSelector((store) => store.search.searchReducer);
   
   const addToCollection = () => {
     dispatch({
@@ -20,17 +19,24 @@ function SearchDetails({ item }) {
 
     const goBack = () => {
         history.push("/search-list");
-      };
-
+    };
+  
     return (
-
       <Box alignItems="center">
         <ul style={{ listStyleType: "none", paddingLeft: 0 }}>
         <li><strong>Common name:</strong> {item?.common_name}</li>
           <li><strong>Other names:</strong> {item?.other_name}</li>
           <li><strong>Watering:</strong> {item?.watering}</li>
-          <li><strong>Sunlight:</strong> {item?.sunlight}</li>
         </ul>
+        <div style={{ display: "flex", flexDirection: "row", justifyContent:"center" }}>
+          <strong>Sunlight:</strong>
+                <ul style={{ listStyleType: "none", paddingLeft: 0 }}>
+                  {/* mapping over sunlight array to display item at each index */}
+                  {item?.sunlight?.map((sunlight, id) => (
+                    <li key={id}>{sunlight}</li>
+                  ))}
+          </ul>
+          </div>
         <Button onClick={goBack} color="success" variant="outlined" size="small">Back</Button>
         <Button onClick={addToCollection} color="success" variant="contained" size="small">Add Plant</Button>
         <Divider component="div" alignSelf="center" sx={{ m: 0.5, p: 0.5}} orientation="horizontal" />
